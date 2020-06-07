ede8280b5e890ae524de84d0d66e5e8d39754fe3
pilyang <pilyang75@gmail.com>
2020-06-07 19:34:59 +0900

    setup

---
+++ src/fgm.cpp
@@ -0,0 +1,5 @@
+#include "msde_fgm/fgm.h"
+
+#include "ros/ros.h"
+#include "ackermann_msgs/AckermannDriveStamped.h"
+#include "sensor_msgs/LaserScan.h"
---
+++ src/fgm_node.cpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include "msde_fgm/fgm.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "msde_fgm");
+    ros::NodeHandle nh;
+
+
+    return 0;
+}
