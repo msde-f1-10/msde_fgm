5a7066b40876950233c00efdb7c9ed113763e61d
pilyang <pilyang75@gmail.com>
2020-06-15 22:55:33 +0900

    drive without reference

--- include/msde_fgm/fgm.h
+++ include/msde_fgm/fgm.h
@@ -10,59 +10,95 @@
 #include "msde_fgm/util.h"
 
 #include <string.h>
+#include <iostream>
+#include <vector>
 
-namespace fgm
-{
+#define PI 3.141592
+#define RACECAR_LENGTH 0.325
 
-    extern float scan_angle_min;
-    extern float scan_angle_max;
-    extern float scan_angle_increment;
-    extern int scan_range_size;
+    namespace fgm
+    {
 
-    class FGM{
-    private:
-        ros::NodeHandle nh_c;
-        ros::Rate loop_rate;
-        ros::Subscriber sub_odom;
-        ros::Subscriber sub_scan;
-        ros::Publisher pub_ack;
-        ros::Subscriber sub_init_scan;
-
-        // parameter for topics
-        std::string scan_topic_name;
-        std::string odom_topic_name;
-        std::string drive_topic_name;
+        // parameter of laser scan data
+        extern float scan_angle_min;
+        extern float scan_angle_max;
+        extern float scan_angle_increment;
+        extern int scan_range_size;
+        extern int range_mid_idx;
 
         // parameter for driving
-        float robot_scale;
-        float threshold;
-        float filter_size;
-        float filter_scale;
+        extern float robot_scale;
+        extern float threshold;
+        extern int gap_size;
+        extern float filter_scale;
 
 
-        // Lidar data
-        float * scan_origin;
-        float * scan_filtered;
-
-        // messages
-        ackermann_msgs::AckermannDriveStamped pub_ack_msg;
-        sensor_msgs::LaserScan scan_data;
-        // message temp
-        //sensor_msgs::MultiEchoLaserScan scan_filter_test;
-        sensor_msgs::LaserScan scan_filter_test;
-        ros::Publisher pub_scan_filtered;
-
-    public:
-        FGM(const ros::NodeHandle h);
-        ~FGM();
-        void start_driving();
-        void subCallback_scan(const sensor_msgs::LaserScan::ConstPtr& msg_sub);
-        void subCallback_odom(const nav_msgs::Odometry::ConstPtr& msg_sub);
-        void sub_initLidarData(const sensor_msgs::LaserScan::ConstPtr& msg_sub);
+        typedef struct _Gap{
+            int start_idx;
+            int end_idx;
+            int max_idx;
+        } Gap;
 
 
+        class FGM{
+        private:
+            ros::NodeHandle nh_c;
+            ros::Rate loop_rate;
+            ros::Subscriber sub_odom;
+            ros::Subscriber sub_scan;
+            ros::Publisher pub_ack;
+            ros::Subscriber sub_init_scan;
+
+            // parameter for topics
+            std::string scan_topic_name;
+            std::string odom_topic_name;
+            std::string drive_topic_name;
+
+            // driving
+            float speed_max;
+            float speed_min;
+
+
+            // Lidar data
+            float * scan_origin;
+            float * scan_filtered;
+
+            // messages
+            ackermann_msgs::AckermannDriveStamped pub_ack_msg;
+            sensor_msgs::LaserScan scan_data;
+            // message temp
+            //sensor_msgs::MultiEchoLaserScan scan_filter_test;
+            sensor_msgs::LaserScan scan_filter_test;
+            ros::Publisher pub_scan_filtered;
+
+        public:
+            FGM(const ros::NodeHandle h);
+            ~FGM();
+            void start_driving();
+            void drive_test(Gap goal);
+            void subCallback_scan(const sensor_msgs::LaserScan::ConstPtr& msg_sub);
+            void subCallback_odom(const nav_msgs::Odometry::ConstPtr& msg_sub);
+            void sub_initLidarData(const sensor_msgs::LaserScan::ConstPtr& msg_sub);
+
+        };
+
+        class GAP{
+        private:
+            std::vector<Gap> gaps;
+            Gap for_gap;
+
+        public:
+            GAP();
+            ~GAP();
+            void gap_finding(float* scan);
+            void print_gapinfo();
+            void for_gap_finding(float* scan);
+            int get_gapsize();
+            Gap get_for_gap();
+            Gap get_maximum_gap();
 
     };
+
 }
 
 
--- src/fgm.cpp
+++ src/fgm.cpp
@@ -14,27 +14,36 @@
 namespace fgm{
 
 
-    float scan_angle_min;
-    float scan_angle_max;
-    float scan_angle_increment;
-    int scan_range_size;
+    float scan_angle_min=0;
+    float scan_angle_max=0;
+    float scan_angle_increment=0;
+    int scan_range_size=0;
+    int range_mid_idx=0;
+
+    // parameter for driving
+    float robot_scale=0;
+    float threshold=0;
+    int gap_size=0;
+    float filter_scale=0;
 
 
     FGM::FGM(const ros::NodeHandle h)
-    :nh_c(h), loop_rate(100)
+    :nh_c(h), loop_rate(200)
     {
         ROS_INFO("start fgm node");
 
         // get parameters for name of topics
         nh_c.getParam("/msde_driving/topic/scan", scan_topic_name);
         nh_c.getParam("/msde_driving/topic/odom", odom_topic_name);
-        nh_c.getParam("/msde_driving/topic/drive", drive_topic_name);
+        nh_c.getParam("/msde_driving/topic/nav", drive_topic_name);
 
         // get parameters for driving configurations
         nh_c.getParam("/msde_driving/driving/robot_scale", robot_scale);
         nh_c.getParam("/msde_driving/driving/threshold", threshold);
-        nh_c.getParam("/msde_driving/driving/filter_size", filter_size);
+        nh_c.getParam("/msde_driving/driving/gap_size", gap_size);
         nh_c.getParam("/msde_driving/driving/filter_scale", filter_scale);
+        nh_c.getParam("/msde_driving/driving/speed_max", speed_max);
+        nh_c.getParam("/msde_driving/driving/speed_min", speed_min);
 
 
         sub_init_scan = nh_c.subscribe(scan_topic_name, 1, &FGM::sub_initLidarData, this);
@@ -70,19 +79,67 @@ namespace fgm{
     void FGM::start_driving()
     {
         ROS_INFO("start driving");
+        GAP gap_obj;
 
         while(ros::ok())
         {
             ros::spinOnce();
+            if(scan_range_size == 0) continue;
             // init ackermann message header
             pub_ack_msg.header.stamp = ros::Time::now();
             pub_ack_msg.header.frame_id = "base_link";
 
+            //find gap
+            gap_obj.gap_finding(scan_filtered);
+            gap_obj.for_gap_finding(scan_filtered);
+            gap_obj.print_gapinfo();
+
+            //test
+            Gap goal_gap = gap_obj.get_maximum_gap();
+            drive_test(goal_gap);
 
             std::cout<<std::endl;
+            //std::system("clear");
             loop_rate.sleep();
         }
     }
+
+
+    void FGM::drive_test(Gap goal)
+    {
+        float steering_angle;
+        float angle = (goal.max_idx - range_mid_idx)*scan_angle_increment;
+        float mid_angle = (goal.start_idx + goal.end_idx)/2;
+        float speed = speed_max;
+
+        float distance = 1;
+        float path_radius = distance / (2 * sin(angle));
+        steering_angle = atan(RACECAR_LENGTH/path_radius);
+        
+
+
+        pub_ack_msg.drive.steering_angle = mid_angle;
+        pub_ack_msg.drive.steering_angle_velocity = 0;
+        pub_ack_msg.drive.speed = speed_min;
+        pub_ack_msg.drive.acceleration = 0;
+        pub_ack_msg.drive.jerk = 0;
+
+        ROS_INFO("speed: %f,  angle: %f", speed, steering_angle);
+        pub_ack.publish(pub_ack_msg);
+
+        int i;
+        scan_filter_test.ranges.clear();
+        for(i=0; i<scan_range_size; i++) {
+            if( i > goal.start_idx && i < goal.end_idx)
+            {
+                scan_filter_test.ranges.push_back(scan_filtered[i]);
+            } else {
+                scan_filter_test.ranges.push_back(0);
+            }
+        }
+        pub_scan_filtered.publish(scan_filter_test);
+    }
+
 
     /* get the laser scan data description */
     void FGM::sub_initLidarData(const sensor_msgs::LaserScan::ConstPtr& msg_sub)
@@ -92,6 +149,7 @@ namespace fgm{
         scan_angle_max = msg_sub -> angle_max;
         scan_angle_increment = msg_sub -> angle_increment;
         scan_range_size = msg_sub -> ranges.size();
+        range_mid_idx = (int)(scan_range_size/2);
 
         // allocate the scan data array
         scan_origin = new float[ scan_range_size ];
@@ -144,8 +202,6 @@ namespace fgm{
             {
                 float unit_length = scan_origin[i] * scan_angle_increment;
                 float filter_num = robot_scale / unit_length;
-                ROS_INFO("r2l unitle size : %f", unit_length);
-                ROS_INFO("r2l filter size : %f", filter_num);
                 int j;
                 for(j=1; j<filter_num+1; j++)
                 {
@@ -164,8 +220,6 @@ namespace fgm{
             {
                 float unit_length = scan_origin[i+1] * scan_angle_increment;
                 float filter_num = robot_scale / unit_length;
-                ROS_INFO("l2r unitle size : %f", unit_length);
-                ROS_INFO("l2r filter size : %f", filter_num);
                 int j;
                 for(j=0; j<filter_num+1; j++)
                 {
@@ -182,6 +236,7 @@ namespace fgm{
 
         } // scan filter end
 
+        // publish filtered scan data
         scan_filter_test.header = msg_sub -> header;
         scan_filter_test.angle_min = scan_angle_min;
         scan_filter_test.angle_max = scan_angle_max;
@@ -190,13 +245,6 @@ namespace fgm{
         scan_filter_test.scan_time = msg_sub -> scan_time;
         scan_filter_test.range_min = msg_sub -> range_min;
         scan_filter_test.range_max = msg_sub -> range_max;
-        scan_filter_test.ranges.clear();
-        for(i=0; i<scan_range_size; i++) {
-            scan_filter_test.ranges.push_back(scan_filtered[i]);
-        }
-
-        pub_scan_filtered.publish(scan_filter_test);
-
 
     }
 
@@ -205,6 +253,153 @@ namespace fgm{
     {
     }
 
+
+    // GAP class
+    GAP::GAP()
+    {
+        this->gaps.reserve(10);
+
+        float theta_for = PI/3;
+        int for_point = (int)(theta_for/scan_angle_increment);
+        this->for_gap.start_idx = (range_mid_idx) - for_point;
+        this->for_gap.end_idx = (range_mid_idx) + for_point;
+    }
+
+    GAP::~GAP()
+    {
+        std::vector<Gap>().swap(gaps);
+    }
+
+    void GAP::gap_finding(float* scan)
+    {
+        // clear previous gaps vector
+        this->gaps.clear();
+        int i, j;
+        for(i=0; i<scan_range_size - gap_size; i++)
+        {
+            if(scan[i] > threshold)
+            {
+                float start_idx_temp = i;
+                float end_idx_temp =i;
+                float max_temp = scan[i];
+                float max_idx_temp = i;
+                // check is gap or not
+                for(j=1; j<gap_size; j++)
+                {
+                    if(scan[++i] > threshold){
+                        if(scan[i] > max_temp){
+                            max_temp = scan[i];
+                            max_idx_temp = i;
+                        }
+                    } else {
+                        max_temp = -1;
+                        break;
+                    } 
+                }//end checking is gap or not
+
+                // is not gap
+                if(max_temp == -1){
+                    break;
+                }
+                // is gap 
+                else{
+                    while( (scan[i] > threshold) && (++i < scan_range_size) )
+                    {
+                        if(scan[i] > max_temp){
+                            max_temp = scan[i];
+                            max_idx_temp = i;
+                        }
+                    }
+                    end_idx_temp = i;
+                    // push the gap data
+                    Gap gap_temp;
+                    gap_temp.start_idx = start_idx_temp;
+                    gap_temp.end_idx = end_idx_temp;
+                    gap_temp.max_idx = max_idx_temp;
+                    this->gaps.push_back(gap_temp);
+                }
+
+            }
+        }// end gap finding loop
+    }// end gap finding method
+
+    void GAP::for_gap_finding(float* scan)
+    {
+        float theta_for = PI/3;
+        int for_point = (int)(theta_for/scan_angle_increment);
+        int start_idx_temp = (range_mid_idx) - for_point;
+        int end_idx_temp = (range_mid_idx) + for_point;
+
+        /*
+        ROS_INFO("%d, %d", start_idx_temp, end_idx_temp);
+        ROS_INFO("%d/ %d", range_mid_idx, scan_angle_increment);
+        */
+
+        int i;
+        int max_idx_temp=start_idx_temp;
+        float max_temp;
+        max_temp = scan[start_idx_temp];
+
+        for(i=start_idx_temp; i<end_idx_temp; i++)
+        {
+            if(max_temp < scan[i]){
+                max_temp = scan[i];
+                max_idx_temp = i;
+            }
+        }
+
+        this->for_gap.start_idx = start_idx_temp; 
+        this->for_gap.end_idx = end_idx_temp;
+        this->for_gap.max_idx = max_idx_temp;
+    }
+
+
+    int GAP::get_gapsize()
+    {
+        return this->gaps.size();
+    }
+
+    Gap GAP::get_for_gap()
+    {
+        return this->for_gap;
+    }
+
+    // get maximum gap
+    // return for_gap if there is no gap in gaps
+    Gap GAP::get_maximum_gap()
+    {
+        int num = this->gaps.size();
+        if( num == 0 ){
+            return this->for_gap;
+        }
+        else{
+            int i;
+            int max_gap_idx = 0;
+            int max_gap_size = this->gaps[0].end_idx - this->gaps[0].start_idx;
+            int temp_gap_size = 0;
+            for(i=0; i<num; i++)
+            {
+                temp_gap_size = this->gaps[i].end_idx - this->gaps[i].start_idx;
+                if(temp_gap_size > max_gap_size){
+                    max_gap_idx = i;
+                    max_gap_size = temp_gap_size;
+                }
+            }
+            return this->gaps[max_gap_idx];
+        }
+
+    }// end get maximum gpa method
+
+    void GAP::print_gapinfo()
+    {
+        ROS_INFO("for gap s: %d, e: %d, max: %d", this->for_gap.start_idx, this->for_gap.end_idx, this->for_gap.max_idx);
+        int num = this->gaps.size();
+        ROS_INFO("number of gap : %d", num);
+        int i;
+        for(i=0; i<num; i++){
+            ROS_INFO("start: %d,  end: %d, maximum: %d", this->gaps[i].start_idx, this->gaps[i].end_idx, this->gaps[i].max_idx);
+        }
+    }
 
 
 }
